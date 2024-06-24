ec5df7e0df364ebc42b9911cd2655a91ac68f4bf
Etienne Wodey <etienne.wodey@aqt.eu>
2024-06-24 19:09:48 +0200

    ksupport: parse device_db in build script

--- artiq/firmware/ksupport/build.rs
+++ artiq/firmware/ksupport/build.rs
@@ -1,6 +1,17 @@
-extern crate build_misoc;
+use std::path::Path;
+use std::{env, fs};
 
 fn main() {
+    let buildinc_dir = env::var("BUILDINC_DIRECTORY").unwrap();
+    let ddb_path = Path::new(&buildinc_dir)
+        .join("generated")
+        .join("device_db.py");
+    println!("cargo:rerun-if-changed={}", ddb_path.to_str().unwrap());
+
+    let ddb = fs::read_to_string(&ddb_path).unwrap();
+    let _ = ddb_parser::parse(&ddb).unwrap();
+    // TODO: emit peripherals instantiation code
+
     build_misoc::cfg();
     println!("cargo:rustc-cfg={}", "ksupport");
 }
