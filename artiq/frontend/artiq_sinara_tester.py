62db4c14402c6e4dd09cfb695437905b227f5980
Etienne Wodey <etienne.wodey@aqt.eu>
2024-07-26 19:23:12 +0200

    sinara_tester: write urukul channel validation window to eeprom

--- artiq/frontend/artiq_sinara_tester.py
+++ artiq/frontend/artiq_sinara_tester.py
@@ -265,10 +265,10 @@ class SinaraTester(EnvExperiment):
         self.core.break_realtime()
         channel.init()
         self.core.break_realtime()
-        sync_delay_seed, _ = channel.tune_sync_delay()
+        sync_delay_seed, window = channel.tune_sync_delay()
         self.core.break_realtime()
         io_update_delay = channel.tune_io_update_delay()
-        return sync_delay_seed, io_update_delay
+        return sync_delay_seed, io_update_delay, window
 
     @kernel
     def setup_urukul(self, channel, frequency):
@@ -314,9 +314,9 @@ class SinaraTester(EnvExperiment):
             else:
                 eeprom = channel_dev.sync_data.eeprom_device
                 offset = channel_dev.sync_data.eeprom_offset
-                sync_delay_seed, io_update_delay = self.calibrate_urukul(channel_dev)
-                print("{}\t{} {}".format(channel_name, sync_delay_seed, io_update_delay))
-                eeprom_word = (sync_delay_seed << 24) | (io_update_delay << 16)
+                sync_delay_seed, io_update_delay, validation_window = self.calibrate_urukul(channel_dev)
+                print("{}\t{} {} {}".format(channel_name, sync_delay_seed, io_update_delay, validation_window))
+                eeprom_word = (sync_delay_seed << 24) | (io_update_delay << 16) | (validation_window << 8)
                 eeprom.write_i32(offset, eeprom_word)
         print("...done")
 
