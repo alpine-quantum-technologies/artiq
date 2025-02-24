435f0f965229560981fe1619af3281bbe9081d95
Etienne Wodey <44871469+airwoodix@users.noreply.github.com>
2025-02-24 14:29:05 +0100

    ksupport: add clear_mock_input_data to kernel API, increase mock data size (#30)
    * ksupport/api: add clear_mock_input_data
    
    * ksupport: increase mock data size per channel to 256

--- artiq/firmware/ksupport/api.rs
+++ artiq/firmware/ksupport/api.rs
@@ -148,6 +148,7 @@ static mut API: &'static [(&'static str, *const ())] = &[
     api!(rtio_output = ::rtio::output),
     api!(rtio_output_wide = ::rtio::output_wide),
     api!(rtio_input_timestamp = ::rtio::input_timestamp),
+    api!(clear_mock_input_data = ::clear_mock_input_data),
     api!(mock_input_data = ::mock_input_data),
     api!(rtio_input_data = ::rtio_input_data),
     api!(rtio_input_timestamped_data = ::rtio::input_timestamped_data),
