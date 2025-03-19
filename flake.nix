8e38f6dcf7d594e9bbeefc09c10c9f2875590fde
Etienne Wodey <etienne.wodey@aqt.eu>
2025-03-19 23:54:39 +0100

    flake: export firmware-only derivation factory

--- flake.nix
+++ flake.nix
@@ -446,7 +446,7 @@
         };
       };
 
-      inherit qtPaths makeArtiqBoardPackage openocd-bscanspi-f;
+      inherit qtPaths makeArtiqBoardPackage makeKasliFirmwarePackage openocd-bscanspi-f;
 
       defaultPackage.x86_64-linux = pkgs.python3.withPackages(ps: [ packages.x86_64-linux.artiq ]);
 
